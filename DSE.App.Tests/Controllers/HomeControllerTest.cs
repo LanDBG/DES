cac7fa821021326d1a220b0a325012d1ccfe1e68
LanDBG <33470948+LanDBG@users.noreply.github.com>
2018-03-09 12:28:07 +0700

    First check in code

---
+++ DSE.App.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DSE.App;
+using DSE.App.Controllers;
+
+namespace DSE.App.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index("a") as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
